4cdb2d98026d6422b4f60802f09a99a2618b6645
EndocrineBAMSc <bamsc.studio@gmail.com>
2022-11-12 08:14:46 +0100

    Basic State Machine implemented

---
+++ RBJ2022/Assets/Scripts/EnumCollection.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnumCollection
+{
+    public enum GameState
+    {
+        MainMenu,
+        Credits,
+        Settings,
+        Quit,
+        GameOver,
+        HighScoreMenu,
+        HighScoreEnd,
+        GameStarting,
+        GameLoop,
+        Pause,
+    }
+
+
+}
---
+++ RBJ2022/Assets/Scripts/GameManager.cs
@@ -0,0 +1,103 @@
+using EnumCollection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameManager : MonoBehaviour
+{
+    #region Fields
+
+    public static GameManager Instance;
+	[SerializeField] private Canvas _mainMenu;
+	[SerializeField] private Canvas _credits;
+	[SerializeField] private Canvas _settings;
+	[SerializeField] private Canvas _pauseMenu;
+	[SerializeField] private Canvas _highscoresStart;
+	[SerializeField] private Canvas _highscoresEnd;
+	[SerializeField] private Canvas _gameOver;
+
+
+	#endregion
+
+	#region Properties
+
+	private GameState _state;
+
+	public GameState State
+	{
+		get { return _state; }
+		private set { _state = value; }
+	}
+
+	#endregion
+
+	#region Public Functions
+
+	public void SwitchState(GameState state)
+	{
+		_state = state;
+
+		switch (_state)
+		{
+			case GameState.MainMenu:
+				Instance._mainMenu.gameObject.SetActive(true);
+                Instance._pauseMenu.gameObject.SetActive(false);
+                Instance._credits.gameObject.SetActive(false);
+                Instance._highscoresEnd.gameObject.SetActive(false);
+                Instance._highscoresStart.gameObject.SetActive(false);
+                Instance._gameOver.gameObject.SetActive(false);
+				Instance._settings.gameObject.SetActive(false);
+                break;
+
+			case GameState.Credits:
+				Instance._mainMenu.gameObject.SetActive(false);
+				Instance._credits.gameObject.SetActive(true);
+				break;
+
+			case GameState.Settings:
+				Instance._settings.gameObject.SetActive(true);
+				break;
+
+			case GameState.GameStarting:
+				break;
+
+			case GameState.GameLoop:
+				break;
+
+			case GameState.GameOver:
+				break;
+
+			case GameState.HighScoreMenu:
+				break;
+
+			case GameState.HighScoreEnd:
+				break;
+
+			case GameState.Pause:
+				break;
+
+			case GameState.Quit:
+
+#if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+#endif
+                Application.Quit();
+				break;
+
+		}
+	}
+
+	#endregion
+
+	#region Private Functions
+	private void Start()
+	{
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	#endregion
+
+
+}
